7b02c20fd9294717451baefaa5d7d0220ec0809b
Dong Hyun Kim <mwave73@gmail.com>
2023-05-29 16:57:48 +0900

    git push origin master

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Training 123')
-
+    print_hi('Training 12345678910')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
