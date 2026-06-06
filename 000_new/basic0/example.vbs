7b9bef8266a5dda19351678a8dffa553423edd02
Eastbadang <71214878+Eastbadang@users.noreply.github.com>
2026-06-06 15:35:50 +0900

    Delete 000_new directory

--- 000_new/basic0/example.vbs
+++
@@ -1,9 +0,0 @@
-Sub Main()
-    Dim i As Integer
-    Dim iSum As Integer
-
-    for i=1 to 100 step 2
-        iSum=iSum+i
-    Next i
-    MsgBox "1~100 중 홀수의 합 =" & iSum
-End Sub
--- 000_new/basic0/test_label.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-
-window = tkinter.Tk() # 루트 윈도우 생성(GUI 객체 생성)
-
-label = tkinter.Label(window, text = "Hello World!") # 라벨 위젯
-label.pack() # 위젯 위치를 적당한 곳에 위치시킨다.(auto)
-
-window.mainloop() # Event loop 시작시켜 GUI를 활성화 시킨다.
